0ad2ad1f99019151ff91c32624e1628261b5eeb8
Christopher Chalcraft <chris.chalcraft@gmail.com>
2022-01-08 20:50:46 -0800

    minimal flake

--- home.nix
+++ home.nix
@@ -9,17 +9,14 @@
     # Feel free to split up your configuration and import pieces of it here.
   ];
 
-  # Comment out if you wish to disable unfree packages for your system
-  nixpkgs.config.allowUnfree = true;
+  xdg.configFile."nix/nix.conf".text = ''
+    experimental-features = nix-command flakes
+  '';
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnsupportedSystem = true;
+  };
 
-  # Enable home-manager and git
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
 
-  # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
 }
