38161258b0ef06f16b4938939eb70b9c3f21c39f
Christopher Chalcraft <chris.chalcraft@gmail.com>
2022-01-31 22:56:32 -0800

    updated changes to vimrc

--- programs/neovim/neovim.nix
+++ programs/neovim/neovim.nix
@@ -4,13 +4,42 @@
     enable = true;
     vimAlias = true;
     plugins = with pkgs.vimPlugins; [
+      #specific languages
       vim-nix
-      vim-colors-solarized
+      vim-markdown
+      vim-elixir
+      vim-go
+      vim-scala
+      rust-vim
+      vim-hcl
+      vim-cue
+      vim-pandoc
+      vim-pandoc-syntax
+      vim-toml
+
+      # config
+      vim-fugitive
       vim-airline
       vim-airline-themes
+
+      #themes
+      vim-colors-solarized
+
+      # utilities
       nerdtree
-      vim-markdown
       ctrlp
+      vim-abolish
+      vim-repeat # cs"'...
+      vim-commentary # gcap
+      vim-indent-object # >aI
+      vim-indent-guides
+      vim-easy-align # vipga
+      vim-eunuch # :Rename foo.rb
+      vim-sneak
+      ale
+
+
+      # extras
       vim-indent-guides
     ];
     extraConfig = builtins.readFile ./neovimrc;
