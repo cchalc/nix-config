a4ead8114da850ed2163da61e3c4f9ce68dc88e0
Christopher Chalcraft <chris.chalcraft@gmail.com>
2024-09-27 10:20:10 -0700

    Update wezterm.lua

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,11 +4,11 @@ return {
 	check_for_updates = false,
 	color_scheme = "terafox",
 	enable_tab_bar = false,
-	font = wezterm.font_with_fallback({
-		"Comic Code",
-		"nonicons",
-	}),
-	font_size = 16,
+	-- font = wezterm.font_with_fallback({
+	-- 	"Comic Code",
+	-- 	"nonicons",
+	-- }),
+	-- font_size = 16,
 	line_height = 1.375,
 	window_decorations = "RESIZE | MACOS_FORCE_DISABLE_SHADOW",
 	window_padding = {
