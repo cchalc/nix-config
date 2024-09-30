f5493c534c4b595ba6f6a782f2d5056c24620938
Chris Chalcraft <chris.chalcraft@gmail.com>
2024-09-30 00:53:24 -0700

    test adding programs

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,14 +1,39 @@
-{pkgs, ...}: {
-  imports = [./direnv ./fish ./git ./nvim];
-  home.packages = with pkgs; [
-    fd
-    jq
-    raycast
-    ripgrep
-    rustup
-    starship
-    stylua
-    yabai
-    zoxide
-  ];
-}
+{pkgs, ...}:
+{
+	imports = 
+	[
+		./direnv
+		./fish
+		./git
+		./nvim
+	];
+
+	home = {
+		packages = with pkgs; [
+			fd
+			jq
+			raycast
+			ripgrep
+			rustup
+			starship
+			stylua
+			yabai
+			zoxide
+			tree
+			htop
+			cachix
+			pandoc
+			whois
+		];
+
+		programs = {
+			fzf = {
+				enable = true;
+			};
+			bat = {
+				enable = true;
+				config.theme - "TwoDark";
+			};
+		};
+	};
+};
