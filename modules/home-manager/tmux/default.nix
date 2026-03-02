151071f6a8765d7a600ba27eb7684eb365994773
Chris Chalcraft <chris.chalcraft@gmail.com>
2026-03-02 00:05:12 -0800

    fixed spacing

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -1,24 +1,26 @@
-programs.tmux = {
-  enable = true;
+{
+  programs.tmux = {
+    enable = true;
 
-  # Options handled natively by Home Manager
-  prefix = "C-s";
-  mouse = true;
-  keyMode = "vi";
-  terminal = "tmux-256color";
+    # Options handled natively by Home Manager
+    prefix = "C-s";
+    mouse = true;
+    keyMode = "vi";
+    terminal = "tmux-256color";
 
-  plugins = with pkgs.tmuxPlugins; [
-    vim-tmux-navigator
+    plugins = with pkgs.tmuxPlugins; [
+      vim-tmux-navigator
 
-    {
-      plugin = catppuccin;
-      extraConfig = ''
-        set -g @catppuccin_window_status_style "rounded"
-        set -g status-left ""
-        set -g status-right "#{E:@catppuccin_status_application} #{E:@catppuccin_status_session}"
-      '';
-    }
-  ];
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_window_status_style "rounded"
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application} #{E:@catppuccin_status_session}"
+        '';
+      }
+    ];
 
-  extraConfig = builtins.readFile ./tmux.conf;
-};
+    extraConfig = builtins.readFile ./tmux.conf;
+  };
+}
