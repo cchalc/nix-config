747e445241117218a8ff557ccbb22ced38d17b1a
Chris Chalcraft <chris.chalcraft@gmail.com>
2026-03-01 23:57:51 -0800

    adding tmux

---
+++ modules/home-manager/tmux/default.nix
@@ -0,0 +1,24 @@
+programs.tmux = {
+  enable = true;
+
+  # Options handled natively by Home Manager
+  prefix = "C-s";
+  mouse = true;
+  keyMode = "vi";
+  terminal = "tmux-256color";
+
+  plugins = with pkgs.tmuxPlugins; [
+    vim-tmux-navigator
+
+    {
+      plugin = catppuccin;
+      extraConfig = ''
+        set -g @catppuccin_window_status_style "rounded"
+        set -g status-left ""
+        set -g status-right "#{E:@catppuccin_status_application} #{E:@catppuccin_status_session}"
+      '';
+    }
+  ];
+
+  extraConfig = builtins.readFile ./tmux.conf;
+};
